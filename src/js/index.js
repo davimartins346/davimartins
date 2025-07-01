69cf38cff01b2f17a828583b10686136d30427e6
davimartins346 <artistadavimartins2025@gmail.com>
2025-06-30 23:48:56 -0300

    subir arquivos

---
+++ src/js/index.js
@@ -0,0 +1,87 @@
+<<<<<<< HEAD
+const botaoFiltrar = document.querySelector('.btn-filtrar');
+
+botaoFiltrar.addEventListener('click', function () {
+  
+  console.log('clicou no botão filtrar');
+
+  const categoriaSelecionada = document.querySelector('#categoria').value;
+
+  const musicas = document.querySelectorAll('.musica');
+
+  musicas.forEach(function (musica) {
+    const categoriaMusica = musica.dataset.categoria;
+
+    let mostrarMusica = true;
+
+    console.log('a categoria selecionada foi: ', categoriaSelecionada);
+    
+
+    const temFiltroDeCategoria = categoriaSelecionada !== '';
+
+    const cartaNaoBateComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaMusica.toLowerCase();
+
+    if (temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria) {
+      mostrarMusica = false;
+    }
+
+    else {
+      mostrarMusica = true;
+    }
+    
+
+    if (mostrarMusica) {
+      musica.classList.add('mostrar');
+      musica.classList.remove('esconder');
+    }
+
+    else{
+      musica.classList.remove('mostrar');
+      musica.classList.add('esconder');
+    }
+  });
+})
+=======
+const botaoFiltrar = document.querySelector('.btn-filtrar');
+
+botaoFiltrar.addEventListener('click', function () {
+  
+  console.log('clicou no botão filtrar');
+
+  const categoriaSelecionada = document.querySelector('#categoria').value;
+
+  const musicas = document.querySelectorAll('.musica');
+
+  musicas.forEach(function (musica) {
+    const categoriaMusica = musica.dataset.categoria;
+
+    let mostrarMusica = true;
+
+    console.log('a categoria selecionada foi: ', categoriaSelecionada);
+    
+
+    const temFiltroDeCategoria = categoriaSelecionada !== '';
+
+    const cartaNaoBateComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaMusica.toLowerCase();
+
+    if (temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria) {
+      mostrarMusica = false;
+    }
+
+    else {
+      mostrarMusica = true;
+    }
+    
+
+    if (mostrarMusica) {
+      musica.classList.add('mostrar');
+      musica.classList.remove('esconder');
+    }
+
+    else{
+      musica.classList.remove('mostrar');
+      musica.classList.add('esconder');
+    }
+  });
+})
+>>>>>>> 2f7408985bb76cd89871b19361ba7c846bed2394
