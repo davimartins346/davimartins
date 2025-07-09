bbc814302d1329461fc670d219b3bedbdada3226
davimartins346 <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>
2025-07-09 01:22:52 -0300

    editei

---
+++ src/js/biografia.js
@@ -0,0 +1,14 @@
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
--- src/js/index.js
+++ src/js/index.js
@@ -40,21 +40,3 @@ botaoFiltrar.addEventListener('click', function () {
     }
   });
 })
-
-
-
-const myObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show')
-    }
-
-    else {
-      entry.target.classList.remove('show')
-    }
-  })
-})
-
-const elements = document.querySelectorAll('.hidden')
-
-elements.forEach((element) => myObserver.observe(element))
